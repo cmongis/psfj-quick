b10daf2e268475e172253b96be5ea3703ac22c5a
cyrilmongis <cyrilmongis@c02xh0jwjgh5.fritz.box>
2018-11-22 19:54:04 +0100

    Introduced reducers afterall

---
+++ src/main/java/psfj/uifx/reducers/Reducer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psfj.uifx.reducers;
+
+import org.scijava.plugin.SciJavaPlugin;
+
+/**
+ *
+ * @author cyrilmongis
+ */
+
+public interface Reducer extends SciJavaPlugin {
+    
+    
+    
+}
---
+++ src/main/java/psfj/uifx/reducers/UiReducer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package psfj.uifx.reducers;
+
+import org.scijava.plugin.Plugin;
+
+/**
+ *
+ * @author cyrilmongis
+ */
+@Plugin(type = Reducer.class)
+public class UiReducer {
+    
+}
